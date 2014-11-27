0a65ed1bb9951609b63a6722c22d0f88ee211b5a
Mark Mansi <mmansi@utexas.edu>
2014-11-26 20:44:04 -0600

    Added signal.h to user libc

---
+++ user/signal.h
@@ -0,0 +1,24 @@
+#ifndef _USER_SIGNAL_H_
+#define _USER_SIGNAL_H_
+
+#define SIGTEST (0)
+#define SIGALRM (1)
+
+typedef struct {
+    unsigned long cr2;
+	unsigned long ds;
+	unsigned long ebp;
+	unsigned long edi;
+	unsigned long esi;
+	unsigned long edx;
+	unsigned long ecx;
+	unsigned long ebx;
+	unsigned long eax;
+	unsigned long eip;
+	unsigned long cs;
+	unsigned long flags;
+	unsigned long esp;
+	unsigned long ss;
+} regs;
+
+#endif
